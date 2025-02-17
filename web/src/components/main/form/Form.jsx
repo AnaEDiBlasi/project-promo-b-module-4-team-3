e65c182976c067efd7d8695f5be58cf4fa4f4823
Tania Salvatella <tsalvatellap@gmail.com>
2025-02-17 17:51:09 +0100

    update form

--- web/src/components/main/form/Form.jsx
+++ web/src/components/main/form/Form.jsx
@@ -73,20 +73,77 @@ const handleClick = (ev) => {
     
     <fieldset className="addForm__group">
       <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-      <input className="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto" onChange={handleChangeInput}/>
-      <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan" onChange={handleChangeInput} />
+      <input 
+      className="addForm__input" 
+      type="text" name="name" 
+      id="name" 
+      placeholder="Nombre del proyecto" 
+      onChange={handleChangeInput}
+      />
+      <input 
+      className="addForm__input" 
+      type="text" 
+      name="slogan" 
+      id="slogan" 
+      placeholder="Slogan" 
+      onChange={handleChangeInput} 
+      />
+
       <div className="addForm__2col">
-        <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio"onChange={handleChangeInput}/>
-        <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Demo"onChange={handleChangeInput}/>
+        <input 
+        className="addForm__input" 
+        type="url" 
+        name="repo" 
+        id="repo" 
+        placeholder="Repositorio"
+        onChange={handleChangeInput}
+        />
+        <input 
+        className="addForm__input" 
+        type="url" 
+        name="demo" 
+        id="demo" 
+        placeholder="Demo"
+        onChange={handleChangeInput}
+        />
       </div>         
-      <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías"onChange={handleChangeInput}/>
-      <textarea className="addForm__input" type="text" name="desc" id="desc" placeholder="Descripción" rows="5" onChange={handleChangeInput}></textarea>
+      <input 
+      className="addForm__input" 
+      type="text" 
+      name="technologies" 
+      id="technologies" 
+      placeholder="Tecnologías"
+      onChange={handleChangeInput}
+      />
+
+      <textarea 
+      className="addForm__input" 
+      type="text" 
+      name="desc" 
+      id="desc" 
+      placeholder="Descripción" 
+      rows="5" 
+      onChange={handleChangeInput}>
+      </textarea>
     </fieldset>
 
     <fieldset className="addForm__group">
       <legend className="addForm__title">Cuéntanos sobre la autora</legend>
-      <input className="addForm__input" type="text" name="autor" id="autor" placeholder="Nombre" onChange={handleChangeInput} />
-      <input className="addForm__input" type="text" name="job" id="job" placeholder="Trabajo" onChange={handleChangeInput} />
+      <input 
+      className="addForm__input" 
+      type="text" 
+      name="autor" 
+      id="autor" 
+      placeholder="Nombre" 
+      onChange={handleChangeInput} 
+      />
+      <input 
+      className="addForm__input" 
+      type="text" 
+      name="job" 
+      id="job" 
+      placeholder="Trabajo" 
+      onChange={handleChangeInput} />
     </fieldset>
 
     <fieldset className="addForm__group--upload">
