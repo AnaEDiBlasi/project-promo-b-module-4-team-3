6839ef5a8b9db95ee7a02d098c2d4b08b513bb33
Gabriella Calvano <gabcalvano@gmail.com>
2025-02-17 17:17:54 +0100

    test projects added

--- src/index.js
+++ src/index.js
@@ -17,7 +17,44 @@ const projectList = [{
     job: "test",
     image: "https://cdn.pixabay.com/photo/2022/05/08/18/30/broken-heart-7182718_1280.png",
     photo: "https://cdn.pixabay.com/photo/2021/08/24/16/34/broken-heart-6571108_1280.png",
-    }];
+    },
+    {
+    name: "Project Test",
+    slogan: "test test test",
+    technologies: "more test",
+    repo: "repo test",
+    demo: "demo test",
+    desc: "test",
+    autor: "test",
+    job: "test",
+    image: "https://cdn.pixabay.com/photo/2022/05/08/18/30/broken-heart-7182718_1280.png",
+    photo: "https://cdn.pixabay.com/photo/2021/08/24/16/34/broken-heart-6571108_1280.png",
+    },
+    {
+    name: "Project Test",
+    slogan: "test test test",
+    technologies: "more test",
+    repo: "repo test",
+    demo: "demo test",
+    desc: "test",
+    autor: "test",
+    job: "test",
+    image: "https://cdn.pixabay.com/photo/2022/05/08/18/30/broken-heart-7182718_1280.png",
+    photo: "https://cdn.pixabay.com/photo/2021/08/24/16/34/broken-heart-6571108_1280.png",
+    },
+    {
+    name: "Project Test",
+    slogan: "test test test",
+    technologies: "more test",
+    repo: "repo test",
+    demo: "demo test",
+    desc: "test",
+    autor: "test",
+    job: "test",
+    image: "https://cdn.pixabay.com/photo/2022/05/08/18/30/broken-heart-7182718_1280.png",
+    photo: "https://cdn.pixabay.com/photo/2021/08/24/16/34/broken-heart-6571108_1280.png",
+    }
+];
 
 //endpoints
 
