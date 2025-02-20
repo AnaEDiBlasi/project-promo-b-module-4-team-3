c5c5e103b3f62be69fe845476ae73ccbf0532261
Ana Elisa <elisaibz92@gmail.com>
2025-02-20 11:56:30 +0100

    DB sql

---
+++ DataBase/tablaautor.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM freedb_poryectosMolones.Autor
+LIMIT 0, 1000
+
+-- Date: 2025-02-20 11:55
+*/
+INSERT INTO `` (`id_Autor`,`autor`,`job`,`image`) VALUES (1,'Emmelie Bjorjlund','Full Stack Developer','srcimagesebook-example.jpg');
+INSERT INTO `` (`id_Autor`,`autor`,`job`,`image`) VALUES (2,'Tania Salvatella','Buscavidas','photo.jpg');
+INSERT INTO `` (`id_Autor`,`autor`,`job`,`image`) VALUES (3,'Ana Elisa Di Blasi','Full Stack Developer','photo.jpg');
+INSERT INTO `` (`id_Autor`,`autor`,`job`,`image`) VALUES (4,'Gabriella Calvano','Full Stack Developer','https://media.geeksforgeeks.org/wp-content/uploads/20240226170303/HoYeon-Jung.webp');
+INSERT INTO `` (`id_Autor`,`autor`,`job`,`image`) VALUES (5,'Patricia Parreira','Front End Developer','https://media.geeksforgeeks.org/wp-content/uploads/20240226170303/HoYeon-Jung.webp');
+INSERT INTO `` (`id_Autor`,`autor`,`job`,`image`) VALUES (6,'Carlos Lopez',NULL,'https://www.example.com/images/fitlife.jpg');
